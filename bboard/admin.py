1e43c9f5dd218e97b532f669d67d3658b810296f
Shchukin Alexandr <qfuzzt@gmail.com>
2021-01-27 20:42:48 +0300

    28.01.21

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Bb
+from .models import Rubric
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title',)
+    search_fields = ('title', 'price', )
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
 
 # Register your models here.
